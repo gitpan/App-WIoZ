23a8e29420d5e7996b2028a8afd54263e821dadd
Yves Agostini <agostini@univ-metz.fr>
2013-08-02 14:08:33 +0000

    Import of YVESAGO/App-WIoZ-0.001 from CPAN.
    gitpan-cpan-distribution: App-WIoZ
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         YVESAGO/App-WIoZ-0.001.tar.gz
    gitpan-cpan-author:       YVESAGO
    gitpan-cpan-maturity:     released

---
+++ lib/App/WIoZ/Point.pm
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+package App::WIoZ;
+BEGIN {
+  $App::WIoZ::VERSION = '0.001';
+}
+
+1;
+
+package App::WIoZ::Point;
+BEGIN {
+  $App::WIoZ::Point::VERSION = '0.001';
+}
+use Moose;
+
+has ['x','y'] => (
+    is => 'rw', isa => 'Int'
+    );
+
+1;
